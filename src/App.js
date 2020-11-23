e3d7609b3fb1fb8320f3ce407707bdc7000096fe
Taek <skylove308@kaist.ac.kr>
2020-11-23 18:18:55 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello React!!
+        </p>
+    </div>
+  );
+}
+
+export default App;
